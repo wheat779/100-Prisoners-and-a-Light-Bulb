69468a21ddf42aa71628b87f35ea6dcf60ae515e
wheat779 <legendarycake@gmail.com>
2014-09-24 09:46:04 -0700

    Initial Commit

---
+++ Prisoner Puzzle Simulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Prisoner Puzzle Simulator
+//
+//  Created by Cameron Kemper on 9/24/14.
+//  Copyright (c) 2014 Cameron Kemper. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
